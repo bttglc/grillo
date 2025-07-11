e87af1ecd28e4eca6121c217138858dc1dd613eb
Gianluca Bottiglieri <bottiglierigianluca@icloud.com>
2025-07-11 15:14:50 +0200

    simplify and revise Task completely

--- src/task.rs
+++ src/task.rs
@@ -1,47 +1,32 @@
-use chrono::{DateTime, Local, NaiveDate};
+use chrono::{DateTime, Utc, NaiveDate};
 
 #[derive(Debug, Clone)]
 pub struct Task {
-    pub id: u64,                            // Only saved tasks have IDs
-    pub description: String,                // The text of the task
-    pub created: DateTime<Utc>,             // For uniqueness purposes
-    pub scheduled: NaiveDate,               // When the task should be done
-    pub deadline: Option<NaiveDate>,        // Not all tasks have deadlines
-    pub status: TaskStatus,                 // v. below
-    pub context: Option<u64>,               // @home, @computer for end user, mapped to ID
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct Draft {
-    pub description: String,                // The text of the task
-    pub created: DateTime<Utc>,             // For uniqueness purposes
-    pub scheduled: NaiveDate,               // When the task should be done
-    pub deadline: Option<NaiveDate>,        // Not all tasks have deadlines
-    pub status: TaskStatus,                 // v. below
-    pub context: Option<u64>,               // @home, @computer for end user, mapped to ID
-}
-
-impl Default for Draft {
-    fn default() -> Draft {
-        Draft {
-            description: String::new(),
-            created: chrono::Utc::now(),
-            scheduled: chrono::Utc::now().date_naive(),
-            deadline: None,
-            status: TaskStatus::default(),
-            context: None,
-        }
-    }
+    pub id: u64,
+    pub description: String,
+    pub created: DateTime<Utc>,
+    pub scheduled: NaiveDate,
+    pub deadline: Option<NaiveDate>,
+    pub status: TaskStatus,
+    pub context: Option<u64>,
 }
 
 #[derive(Debug, Clone, Default)]
 pub enum TaskStatus {
-    Active,
+    #[default] Active,
     Done,
 }
 
-impl Default for TaskStatus {
-    fn default() -> Self {
-        TaskStatus::Active
+impl Task {
+    pub fn new() -> Self {
+        Self {
+            id: 0,                                 // placeholder
+            description: String::new(),
+            created: Utc::now(),
+            scheduled: Utc::now().date_naive(),
+            deadline: None,
+            status: TaskStatus::Active,
+            context: None,
+        }
     }
 }
